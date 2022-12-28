116396364c01473471eeae876d399dea98c32509
Chelle77322 <serenynadarra@gmail.com>
2022-12-28 13:03:18 +1030

    Adding styles and html rendered pages

---
+++ src/pages/Save.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+import Jumbotron from "../components/Jumbo.js";
+import api from "../utils/api.js";
+import SaveContainer from "../components/SaveContainer.js";
+
+class Saved extends Component{
+
+    state= {
+        savedNovels: []
+    }
+    componentDidMount = () =>{
+        this.getNovels()
+    }
+    deleteGoogleNovel = currentNovel => {
+        api.deleteNovel (currentNovel.id).then(result => {
+            console.log("You have removed this book from your saved list", result);
+            this.getNovels();
+        }).catch(error => {
+            console.log("Investigate cause of error",error);
+        })
+    }
+    getNovels = () => {
+        api.getNovels().then (result => {
+            this.setState ({
+                savedNovels: result.data
+            })
+            console.log("This is your result from getNovels", result);
+        }).catch(error => {
+            console.log("Please investigate this error", error);
+        })
+    }
+    render() {
+        return (
+            <div className = "container-fluid blend">
+                <Jumbotron />
+                <h2> Saved Books Page</h2>
+                {this.state.savedNovels.length?(
+                    <SaveContainer
+                    novelState = {this.state.savedNovels}
+                    deleteGoogleNovel = {this.deleteGoogleNovel}></SaveContainer>
+                ): (
+                    <h5>No results to display</h5>
+                )}
+                
+            </div>
+        )
+    }
+}
+export default Saved;
---
+++ src/pages/Search.js
@@ -0,0 +1,68 @@
+import React, {Component} from "react";
+import NovelCard from "../components/NovelCard.js";
+import Row from "../components/Row.js";
+import SearchForm from "../components/SearchForm.js";
+import Jumbotron from "../components/Jumbo.js";
+import api from "../utils/api.js";
+
+class Searched extends Component {
+
+    state = {
+        search: "",
+        results: []
+    };
+//When the page loads initially
+componentDidMount() {
+    console.log("Components successfully mounted");
+    
+}
+
+googleNovels = query => {
+    api.googleNovels(query).then(result => this.setState({results:result.data.items})).then(console.log(this.state.results)).catch(error => console.log(error));
+}
+//When save button is clicked
+handlesaveclick = event => {
+    const novelInfo = event;
+    
+//You then want to save the book to the data base
+api.SaveNovel(novelInfo).then(result => console.log(result)).catch(error => console.log(error));
+}
+handleinputchange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({[name]: value});
+}
+handleformsubmit = event => {
+    event.preventDefault();
+    this.googleNovels(this.state.search);
+}
+render() {
+    return (
+        <div className = "container-fluid five">
+            <Jumbotron />
+            <Row>
+                <SearchForm
+                search = {this.state.search}
+                handleformsubmit = {this.handleformsubmit}
+                handleinputchange = {this.handleinputchange} />
+            </Row>
+            
+            <Row >
+                    {this.state.results.map(novel => ( 
+                        <NovelCard
+                            id={novel.id} 
+                            key={novel.id} 
+                            title={novel.volumeInfo.title} 
+                            description={novel.volumeInfo.description} 
+                            image={novel.volumeInfo.imageLinks.thumbnail} 
+                            link={novel.volumeInfo.infoLink} 
+                            handlesaveclick={this.handlesaveclick}
+                        />
+                    ))}
+            </Row>
+            </div>
+       
+    )
+}
+}
+export default Searched;
